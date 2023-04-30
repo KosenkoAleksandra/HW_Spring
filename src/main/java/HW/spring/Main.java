7fd0ef7bcfe7883253cf98106c87ecb60e9ae5a1
KosenkoAleksandra <kosenkoyes@gmail.com>
2023-04-30 19:59:19 +0300

    Первый коммит

---
+++ src/main/java/HW/spring/Main.java
@@ -0,0 +1,10 @@
+package HW.spring;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
